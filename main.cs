ade59351b2cfa3879b788cda1c78d04a7246e769
danrybar <danielrybar611@gmail.com>
2023-09-11 14:55:56 +0200

    pridal jsem par trid

---
+++ main.cs
@@ -0,0 +1,25 @@
+using System;
+name space SchoolPrep {
+
+
+abstract class GameObject {
+    
+    protected string name;
+    public void Move() {
+
+
+    }
+
+}
+class Snake : GameObject {
+    
+}
+    interface Ire
+}
+class Program {
+    public static void Main (string[] args) {
+        Rectangle r1 = new Rectangle();
+        Square
+    }
+}
+
